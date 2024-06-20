0195fc2198308d230f7cba3285a7bba00eab4bdd
SABRI <132160031+Chihab2023@users.noreply.github.com>
2024-06-20 12:31:39 +0000

    commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""Module for task 0."""
-
-import requests
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers
-    to the subreddit.
-    """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "My-User-Agent"}
-    
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code >= 300:
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
-    
-    return response.json().get("data", {}).get("subscribers", 0)
+
+    return sub_info.json().get("data").get("subscribers")
